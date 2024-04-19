21ed94b7458e131fd43e0290fd0e4c328c0f2a7d
isaias-alt <cascolucasisaias@gmail.com>
2024-04-19 00:56:06 -0400

    prime numbers in a range

---
+++ javascript/prime-numbers-in-a-range.js
@@ -0,0 +1,5 @@
+/**
+ * Exercise 4: Prime Numbers in a Range
+ * Write a program that prompts the user for two positive integers n and m (where n < m) and then prints all prime numbers in the range n to m, 
+ * including n and m if they are prime.
+ */

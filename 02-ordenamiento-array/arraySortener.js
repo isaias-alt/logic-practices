3676712557e26df0d6c7cf1f1a73383fe326c6dc
isaias-at <cascolucasisaias@gmail.com>
2024-05-09 12:16:11 -0400

    arraySortener

--- 02-ordenamiento-array/arraySortener.js
+++ 02-ordenamiento-array/arraySortener.js
@@ -3,7 +3,7 @@
  * Escribe una función en JavaScript que reciba un array de números y devuelva un nuevo array con los elementos ordenados de menor a mayor. 
  * Puedes utilizar cualquier algoritmo de ordenamiento que prefieras, como el algoritmo de burbuja, el de inserción, el de selección o incluso 
  * los métodos integrados en JavaScript como Array.sort().
- */
+*/
 
 let array = [73, 15, 62, 42, 88, 7, 96, 51] // [7, 15, 42, 51, 62, 73, 88, 96]
 
